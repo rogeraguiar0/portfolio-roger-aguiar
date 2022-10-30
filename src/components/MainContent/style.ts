08c90d50b9d0a983de16cf89bcf33538ffeaf1a5
rogeraguiar0 <aguiarroger02@gmail.com>
2022-10-30 15:54:33 -0300

    fix: container spacing

--- src/components/MainContent/style.ts
+++ src/components/MainContent/style.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  min-width: 100vw;
-  width: 100%;
-  max-width: 1000px;
+  width: 100vw;
+  max-width: 100%;
   height: 100%;
   display: flex;
   align-items: center;
